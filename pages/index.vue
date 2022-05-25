b279c093dc4ab9bed0f285bea4d5a855c6af6eb4
JohnnyBoy <theo.lambrt@gmail.com>
2022-05-25 20:18:58 +0200

    feat: clouds animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,32 @@
 <template>
   <section class="bg-sky flex flex-col">
     <div class="container mx-auto lg:px-24 text-center flex py-16 items-center justify-center flex-col">
-      <img class="w-3/4 xl:w-1/2" alt="degenosaurus" src="/main/degenosaurus.png"/>
-      <img class="hidden w-1/6 lg:block absolute left-10 bottom-1/2" alt="degenosaurus" src="/home/Cloud.png"/>
-      <h3 class="mx-12 mt-6 mb-12 lg:mt-12 text-xl xl:text-2xl text-gray-900 font-extrabold text-center">777 Dinos coming to bring fun back to NFT minting !</h3>
-<!--      <div class="container mx-auto text-center">-->
-<!--        <button class="button-82-pushable" role="button">-->
-<!--          <span class="button-82-shadow"></span>-->
-<!--          <span class="button-82-edge"></span>-->
-<!--          <span class="button-82-front text">-->
-<!--          <a>ENTER</a>-->
-<!--        </span>-->
-<!--        </button>-->
-<!--      </div>-->
-      <img class="hidden w-1/5 lg:block absolute right-10 bottom-2/3" alt="degenosaurus" src="/home/Cloud.png"/>
+      <img class="w-3/4 xl:w-1/2 z-50" alt="degenosaurus-logotype" src="/main/degenosaurus.png"/>
+      <h3 class="mx-12 mt-6 mb-12 lg:mt-12 text-xl xl:text-2xl text-gray-900 font-extrabold text-center z-50">777 Dinos
+        coming to bring fun back to NFT minting !</h3>
+
+      <div class="z-0">
+        <div class="w-full absolute left-0 top-0 h-full hidden lg:block animate-cloud">
+          <img class="hidden w-1/6 absolute lg:block left-10 bottom-1/2" alt="cloud1" src="/home/Cloud.png"/>
+          <img class="hidden w-1/5 absolute lg:block right-10 bottom-2/3" alt="cloud2" src="/home/Cloud.png"/>
+        </div>
+
+        <div class="w-full absolute left-0 top-0 h-full hidden lg:block animate-cloud2">
+          <img class="hidden w-1/6 absolute lg:block left-10 bottom-1/2" alt="cloud1" src="/home/Cloud.png"/>
+          <img class="hidden w-1/5 absolute lg:block right-10 bottom-2/3" alt="cloud2" src="/home/Cloud.png"/>
+        </div>
+
+        <div class="w-full absolute left-0 top-0 h-full -ml-1/2 hidden lg:block animate-slowCloud">
+          <img class="hidden w-1/6 absolute lg:block left-10 bottom-1/2" alt="cloud1" src="/home/Cloud.png"/>
+          <img class="hidden w-1/5 absolute lg:block right-10 bottom-2/3" alt="cloud2" src="/home/Cloud.png"/>
+        </div>
+
+        <div class="w-full absolute left-0 top-0 h-full -ml-1/2  hidden lg:block animate-slowCloud2">
+          <img class="hidden w-1/6 absolute lg:block left-10 bottom-1/2" alt="cloud1" src="/home/Cloud.png"/>
+          <img class="hidden w-1/5 absolute lg:block right-10 bottom-2/3" alt="cloud2" src="/home/Cloud.png"/>
+        </div>
+      </div>
     </div>
-<!--    <img class="w-1/6 absolute right-0 bottom-3/4" alt="degenosaurus" src="/home/Cloud.png"/>-->
   </section>
 </template>
 
@@ -25,6 +36,6 @@ import Vue from 'vue'
 export default Vue.extend({
   layout: 'home',
   name: 'Home',
-  props:{},
+  props: {},
 })
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,47 @@ module.exports = {
         'ground': '#81b559',
         'dino-yellow': '#fce354',
         'dino-green': '#729f4e',
-      }
+      },
+      animation: {
+        cloud: 'cloud 60s linear infinite',
+        slowCloud: 'slowCloud 80s linear infinite',
+        cloud2: 'cloud2 60s linear infinite',
+        slowCloud2: 'slowCloud2 80s linear infinite'
+      },
+      keyframes: {
+        cloud: {
+          '0%': {
+            transform: 'translateX(0%)'
+          },
+          '100%': {
+            transform: 'translateX(-100%)'
+          },
+        },
+        slowCloud: {
+          '0%': {
+            transform: 'translateX(50%)'
+          },
+          '100%': {
+            transform: 'translateX(-150%)'
+          },
+        },
+        cloud2: {
+          '0%': {
+            transform: 'translateX(100%)'
+          },
+          '100%': {
+            transform: 'translateX(0%)'
+          },
+        },
+        slowCloud2: {
+          '0%': {
+            transform: 'translateX(50%)'
+          },
+          '100%': {
+            transform: 'translateX(-150%)'
+          },
+        },
+      },
     }
   },
   plugins: [],
